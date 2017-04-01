0aa469b5c989b1410e8a0240e7cfe46ab7605b92
frncsdrk <carlsmoh@gmail.com>
2017-04-01 19:47:59 +0200

    Encapsulate test render code
    add classes to be able to style specrunner output

--- renaissance-test-html-reporter.js
+++ renaissance-test-html-reporter.js
@@ -4,22 +4,46 @@
 define(
     [
         'renaissance'
-        , 'node_modules/renaissance-append-html/appendHTML'
     ]
     , function(renaissance, appendHTML) {
-        // appendHTML drink
-        appendHTML();
-
         function htmlReporter() {
-            function appendWithTest(test, suiteNode) {
-                //
+            function appendWithTest(test, testNode) {
+                // console.log(test);
+                
+                var nameSpan = document.createElement('span');
+                nameSpan.classList.add('test-name');
+                nameSpan.innerHTML = test.name;
+                testNode.appendChild(nameSpan);
+
+                testNode.innerHTML += ' should ';
+
+                var verbSpan = document.createElement('span');
+                verbSpan.classList.add('test-verb');
+                verbSpan.innerHTML = test.verb;
+                testNode.appendChild(verbSpan);
+
+                testNode.innerHTML += ' ';
+
+                var expectedResultSpan = document.createElement('span');
+                expectedResultSpan.classList.add('test-expected-result');
+                expectedResultSpan.innerHTML = String(test.expectedResult);
+                testNode.appendChild(expectedResultSpan);
+
+                if (test.result) {
+                    testNode.classList.add('success');
+                }
+                else {
+                    testNode.classList.add('error');
+                }
             }
             function appendWithSuite(suite) {
                 var body = document.getElementsByTagName('body')[0];
                 var suiteDiv = document.createElement('div');
+                suiteDiv.classList.add('suite-container')
                 body.appendChild(suiteDiv);
                 
                 var header = document.createElement('div');
+                header.classList.add('suite-header');
                 suiteDiv.appendChild(header);
                 
                 var suiteName = document.createElement('h3');
@@ -32,21 +56,14 @@ define(
                 for (var i = 0; i < suite.tests.length; i++) {
                     var item = suite.tests[i];
                     var testDiv = document.createElement('div');
+                    testDiv.classList.add('test-container');
                     suiteBody.appendChild(testDiv);
 
-                    testDiv.innerHTML = item.name + ' should ' + item.verb + ' ' + item.expectedResult;
-                    if (item.result) {
-                        testDiv.style.background = 'green';
-                    }
-                    else {
-                        testDiv.style.background = 'red';
-                    }
+                    appendWithTest(item, testDiv);
                 }
             }
 
             this.report = function(specResults) {
-                console.log(specResults);
-
                 for (var i = 0; i < specResults.length; i++) {
                     appendWithSuite(specResults[i]);
                 }
